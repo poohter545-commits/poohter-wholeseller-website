fb69c8b8f2c3dae9db8ada3747d70eec87d472d2
Poohter <poohter545@gmail.com>
2026-05-15 19:52:29 +0500

    Use live Poohter API domain

--- dist/app.js
+++ dist/app.js
@@ -2,7 +2,7 @@ const API_HOST =
   window.POOHTER_API_HOST ||
   (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://poohter-api.onrender.com");
+    : "https://api.poohter.com");
 const API_BASE = `${API_HOST}/api`;
 const ASSET_BASE = API_BASE.replace("/api", "");
 
